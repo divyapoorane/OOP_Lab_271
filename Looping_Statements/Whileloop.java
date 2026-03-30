17ca1a65af5424d3ddfd805787d0411aa3f68edc
Divya Poorani <rdivyapoorani@gmail.com>
2026-03-30 12:30:49 +0530

    Add files via upload

---
+++ Looping_Statements/Whileloop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Whileloop {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter a number: ");
+int n = sc.nextInt();
+int i=0;
+while(i<=n) {
+System.out.println(n);
+i++;
+}
+}
+}
