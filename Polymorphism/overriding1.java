cea87c0d83892862f12350d9800049bad1da0546
Divya Poorani <rdivyapoorani@gmail.com>
2026-04-08 21:10:56 +0530

    Implement shape classes with method overriding

---
+++ Polymorphism/overriding1.java
@@ -0,0 +1,30 @@
+class Shape {
+    void draw() {
+        System.out.println("Drawing shape");
+    }
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing circle");
+    }
+}
+
+class Rectangle extends Shape {
+    void draw() {
+        System.out.println("Drawing rectangle");
+    }
+}
+
+public class overriding1 {
+    public static void main(String[] args) {
+
+        Shape s;
+
+        s = new Circle();
+        s.draw();
+
+        s = new Rectangle();
+        s.draw();
+    }
+}
