8e7ac581018804d1ff2188cc794196b022db8480
Divya Poorani <rdivyapoorani@gmail.com>
2026-04-08 21:39:08 +0530

    Add files via upload

--- Abstraction/abstraction.java
+++ Abstraction/abstraction.java
@@ -1,1 +1,48 @@
+abstract class Notification {
 
+    // abstract method
+    abstract void send(String message);
+}
+
+// Email Notification
+class EmailNotification extends Notification {
+
+    @Override
+    void send(String message) {
+        System.out.println("Sending Email: " + message);
+    }
+}
+
+// SMS Notification
+class SMSNotification extends Notification {
+
+    @Override
+    void send(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+// Push Notification
+class PushNotification extends Notification {
+
+    @Override
+    void send(String message) {
+        System.out.println("Sending Push Notification: " + message);
+    }
+}
+
+public class abstraction {
+    public static void main(String[] args) {
+
+        Notification n;
+
+        n = new EmailNotification();
+        n.send("Hello via Email");
+
+        n = new SMSNotification();
+        n.send("Hello via SMS");
+
+        n = new PushNotification();
+        n.send("Hello via App Notification");
+    }
+}
